019bd3c03ada08572b3867c62f60c93a36b1cda5
AkhilaBodasu <100083048+AkhilaBodasu@users.noreply.github.com>
2022-02-21 22:40:56 +0530

    Add files via upload
    #TSF GRIP FOUNDATION
    #TASK 5 
    #EDA ON TERROR ATTACK DATA

---
+++ task 5.py
@@ -0,0 +1,260 @@
+﻿# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 21 22:06:49 2022
+
+@author: akhila bodasu
+"""
+
+import math
+import warnings
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import seaborn as sns
+import plotly.offline as py
+import plotly.graph_objs as go
+import matplotlib.pyplot as plt
+warnings.filterwarnings('ignore')
+
+# Let's import to our data and check the basics.
+terror = pd.read_csv('C:\\Users\\akhila bodasu\\Downloads\\globalterrorismdb_0718dist.csv',encoding='ISO-8859-1')
+terror.head()
+terror.columns
+
+terror.rename(columns={'iyear':'Year','imonth':'Month','iday':'Day','country_txt':'Country','provstate':'state',
+                       'region_txt':'Region','attacktype1_txt':'AttackType','target1':'Target','nkill':'Killed',
+                       'nwound':'Wounded','summary':'Summary','gname':'Group','targtype1_txt':'Target_type',
+                       'weaptype1_txt':'Weapon_type','motive':'Motive'},inplace=True)
+# I'm just take important data in whole dataset those I'm using further processing.
+terror=terror[['Year','Month','Day','Country','state','Region','city','latitude','longitude','AttackType','Killed',
+               'Wounded','Target','Summary','Group','Target_type','Weapon_type','Motive']]
+
+
+# Checking the null velues in data
+terror.isnull().sum()
+terror.info()
+
+print("Country with the most attacks:",terror['Country'].value_counts().idxmax())
+print("City with the most attacks:",terror['city'].value_counts().index[1]) #as first entry is 'unknown'
+print("Region with the most attacks:",terror['Region'].value_counts().idxmax())
+print("Year with the most attacks:",terror['Year'].value_counts().idxmax())
+print("Month with the most attacks:",terror['Month'].value_counts().idxmax())
+print("Group with the most attacks:",terror['Group'].value_counts().index[1])
+print("Most Attack Types:",terror['AttackType'].value_counts().idxmax())
+
+from wordcloud import WordCloud
+from scipy import signal
+cities = terror.state.dropna(False)
+plt.subplots(figsize=(10,10))
+wordcloud = WordCloud(background_color = 'green',
+                     width = 512,
+                     height = 384).generate(' '.join(cities))
+plt.axis('off')
+plt.imshow(wordcloud)
+plt.show()
+
+terror['Year'].value_counts(dropna = False).sort_index()
+
+#                           DATA VISUALISATION
+#####   Number of Terrorist Activities each Year
+x_year = terror['Year'].unique()
+y_count_years = terror['Year'].value_counts(dropna = False).sort_index()
+plt.figure(figsize = (18,10))
+sns.barplot(x = x_year,
+           y = y_count_years,
+           palette = 'rocket')
+plt.xticks(rotation = 45)
+plt.xlabel('Attack Year')
+plt.ylabel('Number of Attacks each year')
+plt.title('Attack_of_Years')
+plt.show()
+
+plt.subplots(figsize=(15,6))
+sns.countplot('Year',data=terror,palette='RdYlGn_r',edgecolor=sns.color_palette("YlOrBr", 10))
+plt.xticks(rotation=45)
+plt.title('Number Of Terrorist Activities Each Year')
+plt.show()
+
+
+ #Terrorist Activities by Region in each Year through Area Plot
+pd.crosstab(terror.Year, terror.Region).plot(kind='area',figsize=(15,6))
+plt.title('Terrorist Activities by Region in each Year')
+plt.ylabel('Number of Attacks')
+plt.show()
+
+terror['Wounded'] = terror['Wounded'].fillna(0).astype(int)
+terror['Killed'] = terror['Killed'].fillna(0).astype(int)
+terror['casualities'] = terror['Killed'] + terror['Wounded']
+#  Values are sorted by the top 40 worst terror attacks as to keep the heatmap simple and easy to visualize
+
+terror1 = terror.sort_values(by='casualities',ascending=False)[:40]
+heat=terror1.pivot_table(index='Country',columns='Year',values='casualities')
+heat.fillna(0,inplace=True)
+heat.head()
+
+
+import plotly.offline as py
+py.init_notebook_mode(connected=True)
+import plotly.graph_objs as go
+colorscale = [[0, '#edf8fb'], [.3, '#00BFFF'],  [.6, '#8856a7'],  [1, '#810f7c']]
+heatmap = go.Heatmap(z=heat.values, x=heat.columns, y=heat.index, colorscale=colorscale)
+data = [heatmap]
+layout = go.Layout(
+    title='Top 40 Worst Terror Attacks in History from 1982 to 2016',
+    xaxis = dict(ticks='', nticks=20),
+    yaxis = dict(ticks='')
+)
+fig = go.Figure(data=data, layout=layout)
+py.iplot(fig, filename='heatmap',show_link=False)
+
+
+terror.Country.value_counts()[:15]
+
+
+#Top Countries affected by Terror Attacks¶
+plt.subplots(figsize=(15,6))
+sns.barplot(terror['Country'].value_counts()[:15].index,terror['Country'].value_counts()[:15].values,palette='Blues_d')
+plt.title('Top Countries Affected')
+plt.xlabel('Countries')
+plt.ylabel('Count')
+plt.xticks(rotation= 90)
+plt.show()
+
+###  84% of the terrorist attacks in 1970 were carried out on the American continent. 
+In 1970, the Middle East and North Africa, currently the center of wars and terrorist attacks, faced only one terrorist attack.
+
+Now let us check out which terrorist organizations have carried out their operations in each country. 
+A value count would give us the terrorist organizations that have carried out the most attacks.
+ we have indexed from 1 as to negate the value of 'Unknown' ###
+
+
+
+
+# Total Number of people killed in terror attack
+killData = terror.loc[:,'Killed']
+print('Number of people killed by terror attack:', int(sum(killData.dropna())))# drop the NaN values
+
+# Let's look at what types of attacks these deaths were made of.
+attackData = terror.loc[:,'AttackType']
+# attackData
+typeKillData = pd.concat([attackData, killData], axis=1)
+
+
+typeKillData.head()
+
+typeKillFormatData = typeKillData.pivot_table(columns='AttackType', values='Killed', aggfunc='sum')
+typeKillFormatData
+
+typeKillFormatData.info()
+
+labels = typeKillFormatData.columns.tolist() # convert line to list
+transpoze = typeKillFormatData.T # transpoze
+values = transpoze.values.tolist()
+fig, ax = plt.subplots(figsize=(20, 20), subplot_kw=dict(aspect="equal"))
+plt.pie(values, startangle=90, autopct='%.2f%%')
+plt.title('Types of terrorist attacks that cause deaths')
+plt.legend(labels, loc='upper right', bbox_to_anchor = (1.3, 0.9), fontsize=15) # location legend
+plt.show()
+#Armed assault and bombing/explosion are seen to be the cause of 77% of the deaths in these attacks. 
+This rate is why these attacks are used so many times in terrorist actions. 
+This is how dangerous weapons and explosives are to the world.
+
+#Number of Killed in Terrorist Attacks by Countries
+countryData = terror.loc[:,'Country']
+# countyData
+countryKillData = pd.concat([countryData, killData], axis=1)
+countryKillFormatData = countryKillData.pivot_table(columns='Country', values='Killed', aggfunc='sum')
+countryKillFormatData
+
+
+fig_size = plt.rcParams["figure.figsize"]
+fig_size[0]=25
+fig_size[1]=25
+plt.rcParams["figure.figsize"] = fig_size
+labels = countryKillFormatData.columns.tolist()
+labels = labels[:50] #50 bar provides nice view
+index = np.arange(len(labels))
+transpoze = countryKillFormatData.T
+values = transpoze.values.tolist()
+values = values[:50]
+values = [int(i[0]) for i in values] # convert float to int
+colors = ['red', 'green', 'blue', 'purple', 'yellow', 'brown', 'black', 'gray', 'magenta', 'orange'] # color list for bar chart bar color 
+fig, ax = plt.subplots(1, 1)
+ax.yaxis.grid(True)
+fig_size = plt.rcParams["figure.figsize"]
+fig_size[0]=25
+fig_size[1]=25
+plt.rcParams["figure.figsize"] = fig_size
+plt.bar(index, values, color = colors, width = 0.9)
+plt.ylabel('Killed People', fontsize=20)
+plt.xlabel('Countries', fontsize = 20)
+plt.xticks(index, labels, fontsize=18, rotation=90)
+plt.title('Number of people killed by countries', fontsize = 20)
+# print(fig_size)
+plt.show()
+
+labels = countryKillFormatData.columns.tolist()
+labels = labels[50:101]
+index = np.arange(len(labels))
+transpoze = countryKillFormatData.T
+values = transpoze.values.tolist()
+values = values[50:101]
+values = [int(i[0]) for i in values]
+colors = ['red', 'green', 'blue', 'purple', 'yellow', 'brown', 'black', 'gray', 'magenta', 'orange']
+fig, ax = plt.subplots(1, 1)
+ax.yaxis.grid(True)
+fig_size = plt.rcParams["figure.figsize"]
+fig_size[0]=20
+fig_size[1]=20
+plt.rcParams["figure.figsize"] = fig_size
+plt.bar(index, values, color = colors, width = 0.9)
+plt.ylabel('Killed People', fontsize=20)
+plt.xlabel('Countries', fontsize = 20)
+plt.xticks(index, labels, fontsize=18, rotation=90)
+plt.title('Number of people killed by countries', fontsize = 20)
+plt.show()
+
+
+
+labels = countryKillFormatData.columns.tolist()
+labels = labels[152:206]
+index = np.arange(len(labels))
+transpoze = countryKillFormatData.T
+values = transpoze.values.tolist()
+values = values[152:206]
+values = [int(i[0]) for i in values]
+colors = ['red', 'green', 'blue', 'purple', 'yellow', 'brown', 'black', 'gray', 'magenta', 'orange']
+fig, ax = plt.subplots(1, 1)
+ax.yaxis.grid(True)
+fig_size = plt.rcParams["figure.figsize"]
+fig_size[0]=25
+fig_size[1]=25
+plt.rcParams["figure.figsize"] = fig_size
+plt.bar(index, values, color = colors, width = 0.9)
+plt.ylabel('Killed People', fontsize=20)
+plt.xlabel('Countries', fontsize = 20)
+plt.xticks(index, labels, fontsize=18, rotation=90)
+plt.title('Number of people killed by countries', fontsize = 20)
+plt.show()
+
+
+#Terrorist acts in the Middle East and northern Africa have been seen to have fatal consequences. 
+The Middle East and North Africa are seen to be the places of serious terrorist attacks. 
+In addition, even though there is a perception that Muslims are supporters of terrorism, Muslims are the
+ people who are most damaged by terrorist attacks. If you look at the graphics, it appears that Iraq, Afghanistan and Pakistan 
+ are the most damaged countries. All of these countries are Muslim countries
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
